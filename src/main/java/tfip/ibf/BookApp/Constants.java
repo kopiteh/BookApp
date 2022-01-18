bab27310c17dfdc69fcff0b86350ad38ed6faacf
kopiteh <35788631+kopiteh@users.noreply.github.com>
2022-01-18 16:09:11 +0800

    Commit @ 1609hr

--- src/main/java/tfip/ibf/BookApp/Constants.java
+++ src/main/java/tfip/ibf/BookApp/Constants.java
@@ -2,6 +2,9 @@ package tfip.ibf.BookApp;
 
 public class Constants {
     
+    final public static String THE_BEAN="thebean";
     final public static String API_URL="http://openlibrary.org/search.json";
     final public static String BOOK_DETAIL_URL = "http://openlibrary.org/works/";
+    final public static String ENV_REDIS_PASSWORD = "BOOKAPP_REDIS_PASSWORD";
+
 }
--- src/main/java/tfip/ibf/BookApp/Repository/BookRepository.java
+++ src/main/java/tfip/ibf/BookApp/Repository/BookRepository.java
@@ -1,6 +1,29 @@
 package tfip.ibf.BookApp.Repository;
+import static tfip.ibf.BookApp.Constants.*;
 
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class BookRepository {
     
+    @Autowired
+    @Qualifier(THE_BEAN)
+    private RedisTemplate<String, String> template;
+
+    public void save(String works_id, String value) {
+        template.opsForValue().set(works_id, value, 10L, TimeUnit.MINUTES);
+    }
+
+    public Optional<String> get(String works_id) {
+        String value = template.opsForValue().get(works_id);
+        return Optional.ofNullable(value);
+    }
+
+
 }
